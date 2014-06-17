3c764ed63736b2396e01b9ec03611a3abc47be51
Zubair Desai <zubair.desai@gmail.com>
2014-06-16 18:34:52 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZubairDesai::Application.config.secret_key_base = '7cb97e1cd56179e0f87915724ffd3fa7023eb3a2eed367fea9bf00aea0fb3a7cf709960d348cc758a69cac76352ae80b45e57ec52092af454585510feaf36da1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZubairDesai::Application.config.session_store :cookie_store, key: '_zubair_desai_session'
