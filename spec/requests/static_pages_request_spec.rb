e9feff9a8a83ffef464021e2937ad2a132ec9e33
Zubair Desai <zubair.desai@gmail.com>
2014-06-23 21:18:13 -0700

    Add rspec and bootstrap 3.

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -4,8 +4,8 @@ describe "home page" do
 
   subject {page}
 
-  it "says 'Find me in'" do
+  it "says 'Find me in'" do   
   	visit root_path
-  	expect(page).to have_text("Find me in")
+	expect(page).to have_text("Find me in")
   end
 end
